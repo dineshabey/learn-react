4a3b93e43a29a08ea00fe57a310cb9282d835078
dineshabey <dinabeysinghe@gmail.com>
2023-03-22 12:10:04 +0530

    add gitignore file

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var myH2 = /*#__PURE__*/React.createElement("h2", null, "React Js");
+var appDiv = document.getElementById("app");
+var root = ReactDOM.createRoot(appDiv);
+root.render(myH2);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+const myH2 = <h2>React Js</h2>;
+
+const appDiv = document.getElementById("app");
+
+const root = ReactDOM.createRoot(appDiv);
+
+root.render(myH2);
