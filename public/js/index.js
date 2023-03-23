7df9643466723423551f0be90c4a4904f30cc798
dineshabey <dinabeysinghe@gmail.com>
2023-03-23 13:18:00 +0530

    learned:- add dynamic cmponets

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,39 @@
 "use strict";
 
-var myH2 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " H2 tag worked - React JS "), /*#__PURE__*/React.createElement("p", null, "P tag working "));
+// let studentName1 = "Dinesh Abeysinghe";
+// let studentRegNo1 = "R182321";
+// let studentEmail1 = "dinabeysinghe@gmail.com";
+
+// let studentName2 = "Rasika Pathum";
+// let studentRegNo2 = "R122321";
+// let studentEmail2 = "rasika@gmail.com";
+
+// let student1obj = {
+
+//     name : 'Tharushi Nethmini',
+//     regNo: :'R122321',
+//     email : 'thru@gmail.com'
+// };
+
+// let student2obj = {
+
+//     name : 'Dinesh Abeyrathna',
+//     regNo: :'R172321',
+//     email : 'dinesh@gmail.com'
+// };
+
+var studentsArray = [{
+  name: "Tharushi Nethmini",
+  regNo: "R122321",
+  email: "thru@gmail.com"
+}, {
+  name: "Dinesh Abeyrathna",
+  regNo: "R172321",
+  email: "dinesh@gmail.com"
+}];
+var student1 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[0].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[0].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[0].email));
+var student2 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[1].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[1].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[1].email));
+var student = /*#__PURE__*/React.createElement("div", null, student1, " ", /*#__PURE__*/React.createElement("hr", null), " ", student2, /*#__PURE__*/React.createElement("hr", null));
 var appDiv = document.getElementById("app");
 var root = ReactDOM.createRoot(appDiv);
-root.render(myH2);
+root.render(student);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,63 @@
-const myH2 = (
+// let studentName1 = "Dinesh Abeysinghe";
+// let studentRegNo1 = "R182321";
+// let studentEmail1 = "dinabeysinghe@gmail.com";
+
+// let studentName2 = "Rasika Pathum";
+// let studentRegNo2 = "R122321";
+// let studentEmail2 = "rasika@gmail.com";
+
+// let student1obj = {
+
+//     name : 'Tharushi Nethmini',
+//     regNo: :'R122321',
+//     email : 'thru@gmail.com'
+// };
+
+// let student2obj = {
+
+//     name : 'Dinesh Abeyrathna',
+//     regNo: :'R172321',
+//     email : 'dinesh@gmail.com'
+// };
+
+const studentsArray = [
+  {
+    name: "Tharushi Nethmini",
+    regNo: "R122321",
+    email: "thru@gmail.com",
+  },
+  {
+    name: "Dinesh Abeyrathna",
+    regNo: "R172321",
+    email: "dinesh@gmail.com",
+  },
+];
+
+const student1 = (
   <>
-    <h2> H2 tag worked - React JS </h2>
-    <p>P tag working </p>
+    <h2> Name : {studentsArray[0].name} </h2>
+    <p>Reg No : {studentsArray[0].regNo}</p>
+    <p>Email : {studentsArray[0].email}</p>
   </>
+);
+
+const student2 = (
+  <>
+    <h2> Name : {studentsArray[1].name} </h2>
+    <p>Reg No : {studentsArray[1].regNo}</p>
+    <p>Email : {studentsArray[1].email}</p>
+  </>
+);
+
+const student = (
+  <div>
+    {student1} <hr /> {student2}
+    <hr />
+  </div>
 );
 
 const appDiv = document.getElementById("app");
 
 const root = ReactDOM.createRoot(appDiv);
 
-root.render(myH2);
+root.render(student);
