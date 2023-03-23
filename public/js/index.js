ffdf8b7f96abba4b2e7bbc8ee8cc94fd3b746bdd
dineshabey <dinabeysinghe@gmail.com>
2023-03-23 13:50:38 +0530

    learned:- function dynamic

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,15 @@ var studentsArray = [{
   email: "dinesh@gmail.com",
   marks: "70"
 }];
-var student1 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[0].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[0].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[0].email), /*#__PURE__*/React.createElement("p", null, "Marks : ", studentsArray[0].marks), /*#__PURE__*/React.createElement("p", null, "Result : ", getResult()));
-function getResult() {
-  var mark = 80;
+function getResult(mark) {
   if (mark <= 100 && 0 <= mark) {
-    if (mark <= 80) {
+    if (mark >= 80) {
       return "A";
-    } else if (mark <= 60) {
+    } else if (mark >= 60) {
       return "B";
-    } else if (mark <= 50) {
+    } else if (mark >= 50) {
       return "C";
-    } else if (mark <= 40) {
+    } else if (mark >= 40) {
       return "S";
     } else {
       return "F";
@@ -30,7 +28,8 @@ function getResult() {
     return "Invalid Result";
   }
 }
-var student2 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[1].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[1].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[1].email), /*#__PURE__*/React.createElement("p", null, "Marks : ", studentsArray[1].marks), /*#__PURE__*/React.createElement("p", null, "Result : ", getResult()));
+var student1 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[0].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[0].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[0].email), /*#__PURE__*/React.createElement("p", null, "Marks : ", studentsArray[0].marks), /*#__PURE__*/React.createElement("p", null, "Result : ", getResult(studentsArray[0].marks)));
+var student2 = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, " Name : ", studentsArray[1].name, " "), /*#__PURE__*/React.createElement("p", null, "Reg No : ", studentsArray[1].regNo), /*#__PURE__*/React.createElement("p", null, "Email : ", studentsArray[1].email), /*#__PURE__*/React.createElement("p", null, "Marks : ", studentsArray[1].marks), /*#__PURE__*/React.createElement("p", null, "Result : ", getResult(studentsArray[1].marks)));
 var student = /*#__PURE__*/React.createElement("div", null, student1, " ", /*#__PURE__*/React.createElement("hr", null), " ", student2, /*#__PURE__*/React.createElement("hr", null));
 var appDiv = document.getElementById("app");
 var root = ReactDOM.createRoot(appDiv);
--- src/app.js
+++ src/app.js
@@ -13,26 +13,17 @@ const studentsArray = [
   },
 ];
 
-const student1 = (
-  <>
-    <h2> Name : {studentsArray[0].name} </h2>
-    <p>Reg No : {studentsArray[0].regNo}</p>
-    <p>Email : {studentsArray[0].email}</p>
-    <p>Marks : {studentsArray[0].marks}</p>
-    <p>Result : {getResult()}</p>
-  </>
-);
 
-function getResult() {
-  let mark = 80;
+
+function getResult(mark) {
   if (mark <= 100 && 0 <= mark) {
-    if (mark <= 80) {
+    if (mark >= 80) {
       return "A";
-    } else if (mark <= 60) {
+    } else if (mark >= 60) {
       return "B";
-    } else if (mark <= 50) {
+    } else if (mark >= 50) {
       return "C";
-    } else if (mark <= 40) {
+    } else if (mark >= 40) {
       return "S";
     } else {
       return "F";
@@ -42,13 +33,24 @@ function getResult() {
   }
 }
 
+const student1 = (
+    <>
+      <h2> Name : {studentsArray[0].name} </h2>
+      <p>Reg No : {studentsArray[0].regNo}</p>
+      <p>Email : {studentsArray[0].email}</p>
+      <p>Marks : {studentsArray[0].marks}</p>
+      <p>Result : {getResult(studentsArray[0].marks)}</p>
+    </>
+  );
+
 const student2 = (
   <>
     <h2> Name : {studentsArray[1].name} </h2>
     <p>Reg No : {studentsArray[1].regNo}</p>
     <p>Email : {studentsArray[1].email}</p>
     <p>Marks : {studentsArray[1].marks}</p>
-    <p>Result : {getResult()}</p>
+    <p>Result : {getResult(studentsArray[1].marks)}</p>
+
   </>
 );
 
