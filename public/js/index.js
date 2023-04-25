318510d351d0c62cf3105f6fd733874e79613e4e
dineshabey <dinabeysinghe@gmail.com>
2023-04-25 18:58:23 +0530

    features ES6 :-function spread operator

--- public/js/index.js
+++ public/js/index.js
@@ -26,6 +26,34 @@ var myObject = {
   lName: "Dinesh"
 };
 myNextFunctions(myObject);
+console.log("---------- ES6 Features :- Functions arguments -----------");
+var myArgumentFunc = function myArgumentFunc(a, b) {
+  console.log("a", a);
+  console.log("b", b);
+  for (var _len = arguments.length, restParams = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    restParams[_key - 2] = arguments[_key];
+  }
+  console.log("rest param", restParams);
+  restParams.forEach(function (val) {
+    console.log(val);
+  });
+};
+console.log("---------- ES5 Features :- Functions with array push / concat method -----------");
+myArgumentFunc(10, 20, 30, 40);
+var startNumber = [20, 30, 40];
+var endNumber = [60, 70];
+var _final = [10];
+startNumber.forEach(function (val) {
+  _final.push(val);
+});
+_final.push(50);
+endNumber.forEach(function (val) {
+  _final.push(val);
+});
+console.log(_final);
+console.log("---------- ES6 Features :- Functions with array push / Spread operator )-----------");
+var finalES6 = [10].concat(startNumber, [50], endNumber, [80]);
+console.log(finalES6);
 var appDiv = document.getElementById("app");
 var root = ReactDOM.createRoot(appDiv);
 root.render(view);
--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ myFunction({ fName: "dinesh", lName: "Abeysinghe" });
 
 console.log("---------- ES6 Features :- Functions destructure-----------");
 
-const myNextFunctions = ({fName,lName}) => {
-//   let { fName, lName } = myPara;
+const myNextFunctions = ({ fName, lName }) => {
+  //   let { fName, lName } = myPara;
   console.log(fName);
   console.log(lName);
 };
@@ -19,6 +19,47 @@ const myNextFunctions = ({fName,lName}) => {
 const myObject = { fName: "Charith", lName: "Dinesh" };
 
 myNextFunctions(myObject);
+
+console.log("---------- ES6 Features :- Functions arguments -----------");
+
+const myArgumentFunc = (a, b, ...restParams) => {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("rest param", restParams);
+  restParams.forEach((val) => {
+    console.log(val);
+  });
+};
+
+console.log("---------- ES5 Features :- Functions with array push / concat method -----------");
+
+
+myArgumentFunc(10, 20, 30, 40);
+
+let startNumber = [20, 30, 40];
+let endNumber = [60, 70];
+
+let final = [10];
+
+startNumber.forEach((val) => {
+  final.push(val);
+});
+
+final.push(50);
+
+endNumber.forEach((val) => {
+  final.push(val);
+});
+
+
+console.log(final);
+
+console.log("---------- ES6 Features :- Functions with array push / Spread operator )-----------");
+
+const finalES6 = [10, ...startNumber, 50, ...endNumber, 80];
+
+console.log(finalES6);
+
 
 
 const appDiv = document.getElementById("app");
