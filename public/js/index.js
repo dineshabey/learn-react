54d5c5b51792cfab9c91856caf92a996397c71c9
dineshabey <dinabeysinghe@gmail.com>
2023-04-25 08:59:16 +0530

    object destructuring

--- public/js/index.js
+++ public/js/index.js
@@ -9,31 +9,45 @@ var results = [30, 50, 80];
 var maths = results[0];
 var chemistry = results[1];
 var bio = results[2];
-console.log('maths', maths);
-console.log('chemistry', chemistry);
-console.log('bio', bio);
-console.log('---------- ES6 Features :- Array destructuring -----------');
+console.log("maths", maths);
+console.log("chemistry", chemistry);
+console.log("bio", bio);
+console.log("---------- ES6 Features :- Array destructuring -----------");
 var math1 = results[0],
   chem1 = results[1],
   bio1 = results[2];
-console.log('maths', math1);
-console.log('chemistry', chem1);
-console.log('bio', bio1);
-console.log('---------- ES6 Features :- Variyabale slaping -----------');
+console.log("maths", math1);
+console.log("chemistry", chem1);
+console.log("bio", bio1);
+console.log("---------- ES6 Features :- Variyabale slaping -----------");
 var width = 100;
 var height = 200;
-console.log('--------Normal process-----------');
+console.log("-------- Normal process -----------");
 var tem = width;
 width = height;
 height = tem;
-console.log('width', width);
-console.log('height', height);
-console.log('--------ES6 process-----------');
+console.log("width", width);
+console.log("height", height);
+console.log("-------- ES6 process -----------");
 var _ref = [height, width];
 width = _ref[0];
 height = _ref[1];
-console.log('width', width);
-console.log('height', height);
+console.log("width", width);
+console.log("height", height);
+console.log("---------- ES6 Features :- objet destruturing -----------");
+var student = {
+  Fname: "Dinesh",
+  Lname: "Abeysinghe"
+};
+var firstName = student.Fname;
+var lastName = student.Lname;
+console.log("Last name :- ", firstName);
+console.log("First name :- ", firstName);
+console.log("-------- ES6 process -----------");
+var Fname = student.Fname,
+  Lname = student.Lname;
+console.log("Last name :- ", Fname);
+console.log("First name :- ", Lname);
 var appDiv = document.getElementById("app");
 var root = ReactDOM.createRoot(appDiv);
 root.render(view);
--- src/app.js
+++ src/app.js
@@ -1,48 +1,60 @@
 const view = <div style={{ color: "red" }}> Es6 Features </div>;
 
-
-const results = [30,50,80];
+const results = [30, 50, 80];
 
 let maths = results[0];
 let chemistry = results[1];
 let bio = results[2];
 
-console.log('maths',maths);
-console.log('chemistry',chemistry);
-console.log('bio',bio);
+console.log("maths", maths);
+console.log("chemistry", chemistry);
+console.log("bio", bio);
 
-console.log('---------- ES6 Features :- Array destructuring -----------');
+console.log("---------- ES6 Features :- Array destructuring -----------");
 
-let [math1,chem1,bio1] = results;
+let [math1, chem1, bio1] = results;
 
-console.log('maths',math1);
-console.log('chemistry',chem1);
-console.log('bio',bio1);
+console.log("maths", math1);
+console.log("chemistry", chem1);
+console.log("bio", bio1);
 
-console.log('---------- ES6 Features :- Variyabale slaping -----------');
-
+console.log("---------- ES6 Features :- Variyabale slaping -----------");
 
 let width = 100;
 let height = 200;
 
-console.log('--------Normal process-----------');
-
+console.log("-------- Normal process -----------");
 
 let tem = width;
 width = height;
 height = tem;
 
-console.log('width',width);
-console.log('height',height);
+console.log("width", width);
+console.log("height", height);
 
-console.log('--------ES6 process-----------');
+console.log("-------- ES6 process -----------");
 
-[width,height] = [height,width];
+[width, height] = [height, width];
 
-console.log('width',width);
-console.log('height',height)
+console.log("width", width);
+console.log("height", height);
 
+console.log("---------- ES6 Features :- objet destruturing -----------");
 
+const student = { Fname: "Dinesh", Lname: "Abeysinghe" };
+
+let firstName = student.Fname;
+let lastName = student.Lname;
+
+console.log("Last name :- ", firstName);
+console.log("First name :- ", firstName);
+
+console.log("-------- ES6 process -----------");
+
+let { Fname, Lname } = student;
+
+console.log("Last name :- ", Fname);
+console.log("First name :- ", Lname);
 
 const appDiv = document.getElementById("app");
 const root = ReactDOM.createRoot(appDiv);
