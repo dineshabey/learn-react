cac76b3102fced5e44776c6037a88882da29802c
dineshabey <dinabeysinghe@gmail.com>
2023-05-02 19:57:19 +0530

    feature ES6 :- create classes

--- public/js/index.js
+++ public/js/index.js
@@ -1,100 +1,23 @@
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
-function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 var view = /*#__PURE__*/React.createElement("div", {
   style: {
-    color: "blue"
+    color: "red"
   }
-}, "ES6 Functions");
-console.log("---------- ES6 Features :- Functions -----------");
-var myFunction = function myFunction(myAvg) {
-  console.log(myAvg);
-};
-myFunction({
-  fName: "dinesh",
-  lName: "Abeysinghe"
+}, /*#__PURE__*/React.createElement("h2", null, "This is ES6 Specifications"));
+var student = /*#__PURE__*/_createClass(function student(name, regNo) {
+  _classCallCheck(this, student);
+  this.name = name;
+  this.regNo = regNo;
 });
-console.log("---------- ES6 Features :- Functions destructure-----------");
-var myNextFunctions = function myNextFunctions(_ref) {
-  var fName = _ref.fName,
-    lName = _ref.lName;
-  //   let { fName, lName } = myPara;
-  console.log(fName);
-  console.log(lName);
-};
-var myObject = {
-  fName: "Charith",
-  lName: "Dinesh"
-};
-myNextFunctions(myObject);
-console.log("---------- ES6 Features :- Functions arguments -----------");
-var myArgumentFunc = function myArgumentFunc(a, b) {
-  console.log("a", a);
-  console.log("b", b);
-  for (var _len = arguments.length, restParams = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-    restParams[_key - 2] = arguments[_key];
-  }
-  console.log("rest param", restParams);
-  restParams.forEach(function (val) {
-    console.log(val);
-  });
-};
-console.log("---------- ES5 Features :- Functions with array push / concat method -----------");
-myArgumentFunc(10, 20, 30, 40);
-var startNumber = [20, 30, 40];
-var endNumber = [60, 70];
-var _final = [10];
-startNumber.forEach(function (val) {
-  _final.push(val);
-});
-_final.push(50);
-endNumber.forEach(function (val) {
-  _final.push(val);
-});
-console.log(_final);
-console.log("---------- ES6 Features :- Functions with array push / Spread operator )-----------");
-var finalES6 = [10].concat(startNumber, [50], endNumber, [80]);
-console.log(finalES6);
-console.log("---------- ES6 Features :- Functions with array push / Spread operator )-----------");
-var finalES6Arr = [10, startNumber, 50, endNumber, 80];
-console.log(finalES6Arr);
-console.log("---------- ES6 Features :- Functions object Spread operator )-----------");
-var userName = {
-  fName: "Radika",
-  lName: "Dilanka"
-};
-var cource = {
-  cName: "React",
-  cNo: "1235"
-};
-var address = {
-  city: "Anamaduwa",
-  street: "Kalaniya",
-  postalCode: "454656"
-};
-var fullDetails = _objectSpread(_objectSpread(_objectSpread({}, userName), cource), address);
-console.log(fullDetails);
-console.log("---------- ES7 Features :- key,val)-----------");
-var ingedients = {
-  egs: 4,
-  lemon: 5,
-  sugar: "500g",
-  butter: "1kg"
-};
-console.log(Object.keys(ingedients));
-console.log(Object.values(ingedients));
-console.log(Object.entries(ingedients));
-console.log("---------- ES8 Features :- )-----------");
-var accountNumber = '88012'; //need 10 numbers
-var newAcumber = accountNumber.padStart(10, 0);
-var newAcumberWithX = accountNumber.padEnd(10, 'X');
-console.log(newAcumber);
-console.log(newAcumberWithX);
+var dinesh = new student('Dinesh', '20');
+console.log(dinesh);
 var appDiv = document.getElementById("app");
 var root = ReactDOM.createRoot(appDiv);
 root.render(view);
---
+++ src/app4.js
@@ -0,0 +1,106 @@
+const view = <div style={{ color: "blue" }}>ES6 Functions</div>;
+
+console.log("---------- ES6 Features :- Functions -----------");
+
+const myFunction = (myAvg) => {
+  console.log(myAvg);
+};
+
+myFunction({ fName: "dinesh", lName: "Abeysinghe" });
+
+console.log("---------- ES6 Features :- Functions destructure-----------");
+
+const myNextFunctions = ({ fName, lName }) => {
+  //   let { fName, lName } = myPara;
+  console.log(fName);
+  console.log(lName);
+};
+
+const myObject = { fName: "Charith", lName: "Dinesh" };
+
+myNextFunctions(myObject);
+
+console.log("---------- ES6 Features :- Functions arguments -----------");
+
+const myArgumentFunc = (a, b, ...restParams) => {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("rest param", restParams);
+  restParams.forEach((val) => {
+    console.log(val);
+  });
+};
+
+console.log(
+  "---------- ES5 Features :- Functions with array push / concat method -----------"
+);
+
+myArgumentFunc(10, 20, 30, 40);
+
+let startNumber = [20, 30, 40];
+let endNumber = [60, 70];
+
+let final = [10];
+
+startNumber.forEach((val) => {
+  final.push(val);
+});
+
+final.push(50);
+
+endNumber.forEach((val) => {
+  final.push(val);
+});
+
+console.log(final);
+
+console.log(
+  "---------- ES6 Features :- Functions with array push / Spread operator )-----------"
+);
+
+const finalES6 = [10, ...startNumber, 50, ...endNumber, 80];
+
+console.log(finalES6);
+
+console.log(
+  "---------- ES6 Features :- Functions with array push / Spread operator )-----------"
+);
+
+const finalES6Arr = [10, startNumber, 50, endNumber, 80];
+
+console.log(finalES6Arr);
+
+console.log(
+  "---------- ES6 Features :- Functions object Spread operator )-----------"
+);
+
+let userName = { fName: "Radika", lName: "Dilanka" };
+let cource = { cName: "React", cNo: "1235" };
+let address = { city: "Anamaduwa", street: "Kalaniya", postalCode: "454656" };
+
+let fullDetails = { ...userName, ...cource, ...address };
+console.log(fullDetails);
+
+console.log("---------- ES7 Features :- key,val)-----------");
+
+const ingedients = { egs: 4, lemon: 5, sugar: "500g", butter: "1kg" };
+
+console.log(Object.keys(ingedients));
+console.log(Object.values(ingedients));
+console.log(Object.entries(ingedients));
+
+console.log("---------- ES8 Features :- )-----------");
+
+let accountNumber = '88012'; //need 10 numbers
+let newAcumber = accountNumber.padStart(10,0);
+let newAcumberWithX = accountNumber.padEnd(10,'X');
+
+console.log(newAcumber);
+
+
+console.log(newAcumberWithX);
+
+
+const appDiv = document.getElementById("app");
+const root = ReactDOM.createRoot(appDiv);
+root.render(view);
